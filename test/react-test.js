20e6493ffa3735be285d50571c242120072509ef
Graham Clapham <grahamclapham@Grahams-new-MacBook-Pro.local>
2017-04-16 07:51:01 +0100

    Karma and Jasmine now working correctly.
    Minor changes to tests.

--- test/react-test.js
+++ test/react-test.js
@@ -14,11 +14,6 @@ describe('ReactHeader', function () {
         expect(inst instanceof ReactHeader).toEqual(true);
         expect(wrapper.contains(["This is the default headline"])).toEqual(true);
 
-        //         let wrapper = shallow(
-        //             <ReactHeader />
-        //         );
-        // console.log(wrapper);
-        //        expect(wrapper.contains(<div className="main-header" />)).toEqual(true);
         });
     });
 
